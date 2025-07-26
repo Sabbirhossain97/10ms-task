edda913b708e5ffb02a1afc5a8c862a1cbef2b23
Sabbir Hossain <85300411+Sabbirhossain97@users.noreply.github.com>
2025-07-26 14:22:43 +0600

    fix: background image not rendering in production fixed

--- .next/static/chunks/src_93965a93._.js
+++ .next/static/chunks/src_93965a93._.js
@@ -866,7 +866,7 @@ function CourseTitle() {
     });
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
         id: data === null || data === void 0 ? void 0 : data.title,
-        className: "min-h-[300px] flex flex-col items-center justify-center bg-top md:bg-right bg-cover bg-[url('https://cdn.10minuteschool.com/images/ui_%281%29_1716445506383.jpeg')]",
+        className: "min-h-[300px] flex flex-col items-center justify-center bg-top md:bg-right bg-cover bg-banner",
         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
             className: "max-w-[1200px] relative flex flex-col mx-auto pb-8 md:py-10",
             children: [
